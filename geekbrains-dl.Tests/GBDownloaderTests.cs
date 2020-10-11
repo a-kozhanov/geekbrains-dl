bada31acca141880119ea1d74d3fd7075b9c8a50
a-kozhanov <a.kozhanov@outlook.com>
2020-10-12 01:21:43 +0300

    Добавлен проект для написания тестов

---
+++ geekbrains-dl.Tests/GBDownloaderTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace geekbrains_dl.Tests
+{
+    [TestFixture]
+    public class GBDownloaderTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void GetExtTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ geekbrains-dl.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace geekbrains_dl.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
